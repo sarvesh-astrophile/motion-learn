6f640263cddeabe21c9f671a9a56d303cf89a217
sarvesh-astrophile <sarvesh9301@gmail.com>
2026-04-02 18:53:46 +0530

    learn motion layout

--- src/components/manu/6-layout-part-2.tsx
+++ src/components/manu/6-layout-part-2.tsx
@@ -1,7 +1,49 @@
-default export function LayoutPart2() {
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+export const Navbar = () => {
+  const navItems = [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "About",
+      href: "/about",
+    },
+    {
+      title: "Contact",
+      href: "/contact",
+    },
+    {
+      title: "Login",
+      href: "/login",
+    },
+  ];
+  const [hovered, setHovered] = useState<number | null>(null);
   return (
-    <div>
-       Layout Part 2
+    <div className="py-40">
+      <nav className="max-w-xl mx-auto bg-gray-100 rounded-full py-1 px-1.5 flex">
+        {navItems.map((item, idx) => (
+          <a
+            onMouseEnter={() => setHovered(idx)}
+            onMouseLeave={() => setHovered(null)}
+            key={item.href}
+            href={item.href}
+            className="w-full px-10 relative group py-3 text-lg inline-block text-center text-neutral-700!"
+          >
+            <span className="z-10 relative group-hover:text-neutral-50">
+              {item.title}
+            </span>
+            {hovered === idx && (
+              <motion.div
+                layoutId="nav-hover"
+                className="absolute inset-0 rounded-full w-full h-full bg-neutral-800"
+              ></motion.div>
+            )}
+          </a>
+        ))}
+      </nav>
     </div>
-  )
-}
+  );
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
-import Layouts from "../components/motion/react-tutorial/5-layouts";
+import { Navbar } from "../components/manu/6-layout-part-2";
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
   return (
     <main className="min-h-screen! flex justify-center items-center">
-      <Layouts />
+      <Navbar />
     </main>
   );
 }
