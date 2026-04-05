69e071591edec43fc39bfdda5d69cc767c827969
sarvesh-astrophile <sarvesh9301@gmail.com>
2026-04-05 18:34:00 +0530

    9 portfolio making

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
-import AnimatedSequence2 from "../components/manu/8-animated-sequneces-2";
+import Portfolio from "../components/manu/9-portfolio";
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
   return (
     <main className="min-h-screen! flex justify-center items-center">
-      <AnimatedSequence2 />
+      <Portfolio />
     </main>
   );
 }
